1ff2e9d9cb1eb4539e6b92fcffaec34d7db31b1e
Maria Rafaela <m.rafaela.f@gmail.com>
2022-10-24 20:56:11 -0300

    Criado o People Dto e o Repository

--- simples-crud/src/main/java/br/com/simplescrud/dtos/PeopleDto.java
+++ simples-crud/src/main/java/br/com/simplescrud/dtos/PeopleDto.java
@@ -1,4 +1,18 @@
 package br.com.simplescrud.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class PeopleDto {
+
+    @NotBlank
+    private String completeName;
+    @NotBlank
+    private LocalDate birthDate;
+    @NotBlank
+    private String email;
+
 }
--- simples-crud/src/main/java/br/com/simplescrud/repository/PeopleRepository.java
+++ simples-crud/src/main/java/br/com/simplescrud/repository/PeopleRepository.java
@@ -1,4 +1,13 @@
 package br.com.simplescrud.repository;
 
-public interface PeopleRepository {
+
+import br.com.simplescrud.model.PeopleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<PeopleModel, UUID> {
+
 }
