d36fb2ccc1c034e3fbb7f2b2d849e3145ee969c9
Maria Rafaela <m.rafaela.f@gmail.com>
2022-10-24 20:50:23 -0300

    People Model
    Criado a entidade de pessoas.

--- simples-crud/src/main/java/br/com/simplescrud/model/PeopleModel.java
+++ simples-crud/src/main/java/br/com/simplescrud/model/PeopleModel.java
@@ -1,5 +1,28 @@
 package br.com.simplescrud.model;
 
-public class PeopleModel {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TABLE_OF_PEOPLES")
+public class PeopleModel implements Serializable {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = true, length = 70)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 100)
+    private String completeName;
+    @Column(nullable = false, length = 100)
+    private LocalDate birthDate;
+    @Column(nullable = false, length = 100)
+    private String email;
 
 }
