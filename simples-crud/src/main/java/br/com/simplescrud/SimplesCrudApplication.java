57e74b832fe8238972a9727bfc039772cd57dd9a
Maria Rafaela <m.rafaela.f@gmail.com>
2022-10-24 19:55:46 -0300

    Criando Projeto

---
+++ simples-crud/src/main/java/br/com/simplescrud/SimplesCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.simplescrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesCrudApplication.class, args);
+	}
+
+}
---
+++ simples-crud/src/test/java/br/com/simplescrud/SimplesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simplescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
