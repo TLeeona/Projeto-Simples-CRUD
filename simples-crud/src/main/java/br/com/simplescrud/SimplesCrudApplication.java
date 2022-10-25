adffd2abebe778c8aa2c573425d0b64e4b25410e
Maria Rafaela <m.rafaela.f@gmail.com>
2022-10-25 15:55:22 -0300

    Atualizando propriedades

--- simples-crud/src/main/java/br/com/simplescrud/SimplesCrudApplication.java
+++ simples-crud/src/main/java/br/com/simplescrud/SimplesCrudApplication.java
@@ -1,13 +1,15 @@
 package br.com.simplescrud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "br.com.simplescrud")
 public class SimplesCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimplesCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimplesCrudApplication.class, args);
+    }
 
 }
